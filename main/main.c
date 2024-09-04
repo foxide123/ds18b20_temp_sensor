88cf67d09365f357c572f3b8851e89bbcb24913c
foxide123 <kuba.kolando.02.01@gmail.com>
2024-09-04 16:46:18 +0200

    main function for temperature readings

--- main/main.c
+++ main/main.c
@@ -2,9 +2,34 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "ds18b20.h"
 
-#define ONE_WIRE_PIN GPIO_NUM_4
+void app_main(void)
+{
+    gpio_set_pull_mode(ONE_WIRE_PIN, GPIO_PULLUP_ONLY);
 
-void app_main(void){
+    while (1)
+    {
+        if (one_wire_reset())
+        {
+            ESP_LOGI("1-Wire", "Device found");
 
+            one_wire_write_byte(0xCC);
+            one_wire_write_byte(0x44);
+
+            vTaskDelay(pdMS_TO_TICKS(750));
+
+            one_wire_reset();
+            one_wire_write_byte(0xCC);
+            one_wire_write_byte(0xBE);
+
+            uint8_t tempLSB = one_wire_read_byte();
+            uint8_t tempMSB = one_wire_read_byte();
+            int16_t temp = (tempMSB << 8) | tempLSB;
+
+            ESP_LOGI("1-Wire", "Temperature: %d.%04d C", temp/16, (temp%16)*625);
+        }else{
+            ESP_LOGI("1-Wire", "No device found");
+        }
+    }
 }
