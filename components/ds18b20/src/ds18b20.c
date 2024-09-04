ca861b600066d70517ef631ab49b4a06daf3bd4f
foxide123 <kuba.kolando.02.01@gmail.com>
2024-09-04 16:24:47 +0200

    write and read bit ds18b20

--- components/ds18b20/src/ds18b20.c
+++ components/ds18b20/src/ds18b20.c
@@ -7,6 +7,7 @@
 bool one_wire_reset(void) {
     bool isPresent = false;
 
+    // Configure the pin as output
     gpio_set_direction(ONE_WIRE_PIN, GPIO_MODE_OUTPUT);
     gpio_set_level(ONE_WIRE_PIN, 0);    
     esp_rom_delay_us(480);                      
@@ -14,9 +15,41 @@ bool one_wire_reset(void) {
     gpio_set_direction(ONE_WIRE_PIN, GPIO_MODE_INPUT);  // Release the bus line
     esp_rom_delay_us(70);                       
 
-    isPresent = !gpio_get_level(ONE_WIRE_PIN);  // Check presence pulse (should be low if device is present)
+    isPresent = !gpio_get_level(ONE_WIRE_PIN);  // Check for presence pulse (should be low if device is present)
     esp_rom_delay_us(410);                      // Wait for the remainder of the reset period (total period time - 960 microseconds)
  
     return isPresent;
 }
 
+void one_wire_write_bit(bool bit){
+    gpio_set_direction(ONE_WIRE_PIN, GPIO_MODE_OUTPUT);
+
+    if(bit){
+        gpio_set_level(ONE_WIRE_PIN, 0);
+        esp_rom_delay_us(10);
+        gpio_set_level(ONE_WIRE_PIN, 1);
+        esp_rom_delay_us(55);
+    } else {
+        gpio_set_level(ONE_WIRE_PIN, 0);
+        esp_rom_delay_us(65);
+        gpio_set_level(ONE_WIRE_PIN, 1);
+    }
+
+    esp_rom_delay_us(5);
+}
+
+bool one_wire_read_bit(void){
+    bool bit;
+
+    gpio_set_direction(ONE_WIRE_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(ONE_WIRE_PIN, 0);
+    esp_rom_delay_us(3);
+
+    gpio_set_direction(ONE_WIRE_PIN, GPIO_MODE_INPUT);
+    esp_rom_delay_us(10);
+    
+    bit = gpio_get_level(ONE_WIRE_PIN);
+    esp_rom_delay_us(50);
+
+    return bit;
+}
